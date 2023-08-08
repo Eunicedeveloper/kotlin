f1fedc448006b319a96674bcfc3181bbab0ced1d
dcccalvin <fakecalvin8@gmail.com>
2023-08-08 09:40:15 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    //variables in kotlin
+    var name = "kim"
+    var age = 78
+    println(name)
+    println(age)
+
+    //way two variables in kotlin
+    var city: String = "Nairobi"
+    var idnum: Int = 73459
+    println(city)
+    println(idnum)
+}
---
+++ src/main/kotlin/varfile.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var num1 = 20
+    var num2 = 26
+    var result1 = num1 + num2
+    println(result1)
+
+    var result2 = result1 * 5
+    println(result2)
+
+    var result3 = result2/result1
+    var result4 = result3*result3
+    println(result4)
+}
